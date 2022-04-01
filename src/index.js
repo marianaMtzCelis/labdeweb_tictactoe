972bd58cd9a10a0be4fd5518a95117bd333313a8
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2022-03-31 19:44:24 -0600

    Tracking game moves

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css"
 
 var player = { score: 1, name: "Jeff" }
 player.score = 2
+const numbers = [1, 2, 3]
+const doubled = numbers.map(x => x * 2)
 
 function Square(props) {
   return (
@@ -82,6 +84,15 @@ class Game extends React.Component {
     const current = history[history.length - 1]
     const winner = calculateWinner(current.squares)
 
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start"
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status
     if (winner) {
       status = "Winner: " + winner
@@ -96,7 +107,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     )
